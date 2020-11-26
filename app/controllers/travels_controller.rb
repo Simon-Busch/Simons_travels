4bd4dec0d695ef16fedfa0bc2f983665b7883717
Simon Busch <simon.busch@live.fr>
2020-11-26 07:59:10 +0100

    add geocoding module

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -5,6 +5,15 @@ class TravelsController < ApplicationController
   # GET /travels.json
   def index
     @travels = Travel.all
+
+    @markers = @travels.geocoded.map do |travel|
+      {
+        lat: travel.latitude,
+        lng: travel.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { travel: travel }),
+        image_url: helpers.asset_url('https://www.flaticon.com/svg/static/icons/svg/715/715696.svg')
+      }
+    end
   end
 
   # GET /travels/1
